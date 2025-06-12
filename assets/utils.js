81d934503aeecffafc0dc4b6698547672bca9d98
Mohammad <mohammad.dahamshi@gmail.com>
2025-06-12 19:55:56 +0300

    Adds utils

---
+++ assets/utils.js
@@ -0,0 +1,60 @@
+
+const INFO = {
+    site: 'https://sarawebs.com',
+    company: 'SaraWebs',
+    year: new Date().getFullYear()
+
+}    
+/**
+ * 
+ * @param {string} prefix prefix to add before the generated id
+ * @returns random id with prefix
+ * 
+ * example: generateID('myapp') : retruns 'myapp-XXXXXXXX'
+ * if no prefix provided: 'XXXXXXXXX'
+ */
+function generateID(prefix = ''){
+    prefix = (prefix ? prefix + '-' : prefix);
+    return prefix + crypto.randomUUID().toString().substring(0,8);
+}
+/**
+ * 
+ * @param {String} title title of the app
+ * Function to add copyright to footer, uses the const INFO site as website
+ * company as company name
+ */
+function addCopyRight(title = 'This WebSite'){
+    const div = document.createElement('div');
+    const p = document.createElement('p');
+    p.style.textAlign = 'center';
+    p.innerHTML = `
+    ${title} © ${INFO.year}<br>Built with love by 
+    <a href="${INFO.site}" style="color: #207de9;text-decoration: none;">${INFO.company}</a>
+    `;
+    
+    div.appendChild(p);
+    const footer = document.querySelector('footer');
+    footer.appendChild(div);
+
+}
+
+/**
+ * 
+ * @param {Number} rows number of rows
+ * @param {Number} cols number of columns
+ * @param {Function} pushFunc function to apply on each row cols times
+ * example: (boardRow) => {boardRow.push(new Cell())}
+ * @returns board[rows][cols]
+ */
+function createBoard(rows, cols, pushFunc){
+    let board = [];
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < cols; j++) {
+            pushFunc(board[i]);
+        }
+    }
+    return board;
+}
+const sb_utils = {generateID, addCopyRight, createBoard};
+export {sb_utils};
